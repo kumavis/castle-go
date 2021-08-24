2f42bd31a49d6bf74177a412f4be26fac3136bad
kumavis <aaron@kumavis.me>
2021-08-23 18:53:15 -0700

    so we are at war

--- index.js
+++ index.js
@@ -28,22 +28,23 @@ let gameState = createGameState()
 const sceneUpdaters = {
   add (id, modelType, modelState, patch) {
     // console.log('add', id, patch)
-    const castle = models[modelType].object.clone()
-    castle.gameId = id
-    scene.add(castle)
-    castle.position.x = modelState.x * 10
-    castle.position.z = modelState.y * 10
+    const obj = models[modelType].object.clone()
+    obj.gameId = id
+    scene.add(obj)
+    obj.position.x = modelState.x * 10
+    obj.position.z = modelState.y * 10
+    obj.rotation.y = modelState.direction/4 * fullTurn
   },
   replace (id, modelType, modelState, patch) {
     // console.log('replace', id, patch)
-    const castle = scene.getObjectByProperty('gameId', id)
-    castle.position.x = modelState.x * 10
-    castle.position.z = modelState.y * 10
+    const obj = scene.getObjectByProperty('gameId', id)
+    obj.position.x = modelState.x * 10
+    obj.position.z = modelState.y * 10
   },
   remove (id, modelType, modelState, patch) {
     // console.log('remove', id, patch)
-    const castle = scene.getObjectByProperty('gameId', id)
-    scene.remove(castle)
+    const obj = scene.getObjectByProperty('gameId', id)
+    scene.remove(obj)
   },
 }
 
@@ -59,18 +60,26 @@ for (const snapshot of gameHistory) {
   board = new GoBoard(snapshot)
   gameState = boardToGameState(board)
   updateScene()
-  await timeout(500)
+  await timeout(100)
 }
 
 
 function createGameState () {
   return {
-    'castle-white': {},
-    'castle-black': {},
+    'castle-tower-white': {},
+    'castle-tower-black': {},
+    'castle-wall-white': {},
+    'castle-wall-black': {},
+    'castle-tee-white': {},
+    'castle-tee-black': {},
+    'castle-corner-white': {},
+    'castle-corner-black': {},
     'tent-white': {},
     'tent-black': {},
     'knight-white': {},
     'knight-black': {},
+    'mangonel-white': {},
+    'mangonel-black': {},
   }  
 }
 
@@ -141,6 +150,10 @@ function getChainNeighbors (board, chain) {
 
 function createTerritoryBoard (board) {
   const territoryBoard = Array(board.height).fill().map(() => Array(board.width).fill(0))
+  const hasBlack = board.signMap.some(row => row.some(stone => stone === 1))
+  const hasWhite = board.signMap.some(row => row.some(stone => stone === -1))
+  // territory requires each player to have player at least once (actually twice?)
+  if (!hasBlack || !hasWhite) return territoryBoard
   const emptyChains = getEmptyChains(board)
   for (const chain of emptyChains) {
     const neighbors = getChainNeighbors(board, chain)
@@ -168,27 +181,105 @@ function getFriendlyNeighborCount (board, vertex) {
 function boardToGameState (board) {
   const gameState = createGameState()
   const libertyBoard = createLibertyBoard(board)
+  const territoryBoard = createTerritoryBoard(board)
   visitBoard(board, (vertex, stoneValue) => {
-    let modelType, color
+    let modelType, color, direction = 0
     const [x,y] = vertex
     if (stoneValue === 0) {
-      const libertyValue = libertyBoard[y][x]
-      // no liberty
-      if (libertyValue === undefined) return
-      // shared liberty
-      if (libertyValue === 0) return
-      modelType = 'knight'
-      color = libertyValue === -1 ? 'black' : 'white'
+      // no stone, check liberty
+      const territoryValue = territoryBoard[y][x]
+      if (territoryValue !== 0) {
+        modelType = 'mangonel'
+        color = territoryValue === -1 ? 'black' : 'white'
+      } else {
+        const libertyValue = libertyBoard[y][x]
+        // no liberty
+        if (libertyValue === undefined) return
+        // shared liberty
+        if (libertyValue === 0) return
+        modelType = 'knight'
+        color = libertyValue === -1 ? 'black' : 'white'
+      }
     } else {
-      const strength = getFriendlyNeighborCount(board, vertex)
-      modelType = strength > 0 ? 'castle' : 'tent'
       color = stoneValue === -1 ? 'black' : 'white'
+      const [shape, shapeDir] = getShapeByNeighbors(board, vertex)
+      direction = shapeDir
+      modelType = modelTypeForShape(shape)
     }
     const id = `${modelType}-${color}-${x}-${y}`
-    gameState[`${modelType}-${color}`][id] = { x, y }
+    gameState[`${modelType}-${color}`][id] = { x, y, direction }
   })
 
   return gameState
+}
+
+function modelTypeForShape (shape) {
+  switch (shape) {
+    case 'lone': {
+      // const strength = getFriendlyNeighborCount(board, vertex)
+      return 'tent'
+    }
+    case 'single': {
+      return 'castle-tower'
+    }
+    case 'straight': {
+      return 'castle-wall'
+    }
+    case 'corner': {
+      return 'castle-corner'
+    }
+    case 'tee': {
+      return 'castle-tee'
+    }
+    default: {
+      return 'castle-tower'
+    }
+  }
+}
+
+// "fixed" means this will return spaces even if they are off the board
+function getMatchingNeighborsFixed (board, [x, y]) {
+  const stoneValue = board.get([x, y])
+  // we map instead of filter to maintain shape
+  const neighbors = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+    .map(v => board.has(v) && board.get(v) === stoneValue)
+  return neighbors
+}
+
+function getShapeByNeighbors (board, vertex) {
+  const neighbors = getMatchingNeighborsFixed(board, vertex)
+  const count = neighbors.filter(Boolean).length
+
+  switch (count) {
+    case 0: {
+      return ['lone', 0]
+    }
+    case 1: {
+      const direction = [2,0,1,3][neighbors.indexOf(true)]
+      return ['single', direction]
+    }
+    case 2: {
+      const acrossX = (neighbors[0] && neighbors[1])
+      if (acrossX) return ['straight', 0]
+      const acrossY = (neighbors[2] && neighbors[3])
+      if (acrossY) return ['straight', 1]
+      const direction =
+        (neighbors[0] && neighbors[2]) ? 2 :
+        (neighbors[0] && neighbors[3]) ? 3 :
+        (neighbors[1] && neighbors[2]) ? 1 : 0
+      return ['corner', direction]
+    }
+    case 3: {
+      const direction = [1,3,0,2][neighbors.indexOf(false)]
+      return ['tee', direction]
+    }
+    case 4: {
+      return ['all', 0]
+    }
+    default: {
+      return ['lone', 0]
+    }
+  }
 }
 
 function init() {
@@ -241,9 +332,14 @@ async function loadAllAssets () {
   const blueColor = { r: 80/255, g: 133/255, b: 188/255 }
   await Promise.all([
     loadAsset('tent-white', 'models/nature/Models/OBJ format/tent_detailedOpen').promise,
-    loadAsset('castle-black', 'models/tower-defense/Models/OBJ format/towerSquare_sampleA').promise,
+    loadAsset('castle-tower-black', 'models/castle/Models/towerSquareRoof').promise,
+    loadAsset('castle-wall-black', 'models/castle/Models/wall').promise,
+    loadAsset('castle-tee-black', 'models/castle/Models/wallHalf').promise,
+    loadAsset('castle-corner-black', 'models/castle/Models/wallCorner').promise,
+    loadAsset('flag-black', 'models/castle/Models/flagBlue').promise,
     loadAsset('knight-black', 'models/castle/Models/knightBlue').promise,
     loadAsset('knight-white', 'models/castle/Models/knightRed').promise,
+    loadAsset('mangonel-black', 'models/castle/Models/siegeCatapult').promise,
     loadAsset('ground-grass', 'models/nature/Models/OBJ format/ground_grass').promise,
     loadAsset('cliff-top-rock', 'models/nature/Models/OBJ format/cliff_top_rock').promise,
     loadAsset('plant-bush-small', 'models/nature/Models/OBJ format/plant_bushSmall').promise,
@@ -255,19 +351,47 @@ async function loadAllAssets () {
   models['plant-bush-small'].object.scale.multiplyScalar(10)
   models['plant-lily-large'].object.scale.multiplyScalar(10)
   models['cliff-top-rock'].object.scale.multiplyScalar(10)
-  models['knight-white'].object.rotation.y = 1/4 * fullTurn
-  models['knight-black'].object.rotation.y = 3/8 * fullTurn
-  models['castle-black'].object.scale.multiplyScalar(10)
+  // models['knight-white'].object.children[1].rotation.y = 1/4 * fullTurn
+  models['knight-black'].object.children[1].rotation.y = 3/8 * fullTurn
+  models['castle-tower-black'].object.children[0].position.x += 5
+  models['castle-tower-black'].object.children[0].position.z -= 5
+  models['castle-wall-black'].object.children[0].rotation.y = 1/4 * fullTurn
+  models['castle-wall-black'].object.children[0].position.x += 5
+  models['castle-wall-black'].object.children[0].position.z += 5
+  models['castle-tee-black'].object.children[0].rotation.y = 3/4 * fullTurn
+  models['castle-tee-black'].object.children[0].position.x -= 5
+  models['castle-tee-black'].object.children[0].position.z -= 5
+  models['castle-corner-black'].object.children[0].rotation.y = 3/4 * fullTurn
+  models['castle-corner-black'].object.children[0].position.x -= 5
+  models['castle-corner-black'].object.children[0].position.z -= 5
+  models['flag-black'].object.scale.multiplyScalar(1.5)
+  models['flag-black'].object.position.y += 10
+  models['mangonel-black'].object.scale.multiplyScalar(0.6)
   // modify the asset to have 2 color versions
-  models['tent-black'] = { ...models['tent-white'], object: models['tent-white'].object.clone() }
-  cloneTextures(models['tent-black'].object)
+  models['mangonel-white'] = cloneModel(models['mangonel-black'])
+  models['flag-white'] = cloneModel(models['flag-black'])
+  models['flag-white'].object.children[1].material.color = redColor
+  models['flag-white'].object.children[0].material[1].color = redColor
+  models['castle-tower-white'] = cloneModel(models['castle-tower-black'])
+  models['castle-tower-white'].object.children[0].material[2].color = redColor
+  models['castle-wall-white'] = cloneModel(models['castle-wall-black'])
+  models['castle-tee-white'] = cloneModel(models['castle-tee-black'])
+  models['castle-corner-white'] = cloneModel(models['castle-corner-black'])
+  models['tent-black'] = cloneModel(models['tent-white'])
   models['tent-black'].object.children[0].material[0].color = blueColor
-  models['castle-white'] = { ...models['castle-black'], object: models['castle-black'].object.clone() }
-  // deep clone all materials
-  cloneTextures(models['castle-white'].object)
-  // change primary colors
-  models["castle-black"].object.children[0].material[1].color = blueColor
-  models["castle-white"].object.children[0].material[1].color = redColor
+  models['mangonel-white'].object.children[2].material[0].color = redColor
+  models['mangonel-white'].object.children[3].material[0].color = redColor
+  models['mangonel-white'].object.children[0].material[2].color = redColor
+
+  // compose
+  models['castle-corner-white'].object.add(models['flag-white'].object.clone())
+  models['castle-corner-black'].object.add(models['flag-black'].object.clone())
+}
+
+function cloneModel (model) {
+  const cloned = { ...model, object: model.object.clone() }
+  cloneTextures(cloned.object)
+  return cloned
 }
 
 function cloneTextures (object) {
@@ -401,4 +525,8 @@ function deferred () {
     reject = _reject
   })
   return { resolve, reject, promise }
+}
+
+function vertexEquals([x1, y1], [x2, y2]) {
+  return x1 === x2 && y1 === y2
 }
